e19a407d2a691286210346a07d98afcb2cd3a9b6
Mahesh.Amireddy <maheshreddyedu32825@gmail.com>
2025-12-13 23:54:06 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import rss from '@astrojs/rss';
 
 export default defineConfig({
-  site: 'https://yourdomain.com', // set your URL
-  integrations: [sitemap(), rss()],
-  markdown: {
-    syntaxHighlight: 'prism'
-  }
+  site: 'https://maheshreddy32825.github.io'
 });
