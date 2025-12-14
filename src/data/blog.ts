1203c9a11918d6d087527f549573700f1a7a9f3b
Mahesh.Amireddy <maheshreddyedu32825@gmail.com>
2025-12-14 00:49:54 -0600

    Create blog.ts

---
+++ src/data/blog.ts
@@ -0,0 +1,9 @@
+// src/data/blog.ts
+export const blogPosts = [
+  {
+    title: "How I Deploy Tetris on Kubernetes",
+    url: "https://medium.com/devops-dev/aws-project-1-a-step-by-step-guide-to-deploying-e-commerce-apparel-app-on-amazon-eks-6bd4ecdb183f",
+    summary: "A Step-by-Step Guide to Deploying an E-commerce Apparel App on EKS",
+    date: "2024-10-06"
+  }
+];
