e61694cbeb0fbe61ae2c306b999c67ad15163b7c
Mahesh.Amireddy <maheshreddyedu32825@gmail.com>
2025-12-14 01:09:45 -0600

    Update blog.ts

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,9 +1,16 @@
 // src/data/blog.ts
 export const blogPosts = [
   {
-    title: "How I Deploy Tetris on Kubernetes",
+    title: "Deploying an E-commerce Apparel App on EKS",
     url: "https://medium.com/devops-dev/aws-project-1-a-step-by-step-guide-to-deploying-e-commerce-apparel-app-on-amazon-eks-6bd4ecdb183f",
     summary: "A Step-by-Step Guide to Deploying an E-commerce Apparel App on EKS",
-    date: "2024-10-06"
+    date: "2024-07-11"
+  },
+  {
+    title: "Understanding DevSecOps Pipelines",
+    url: "https://medium.com/@yourhandle/understanding-devsecops-pipelines-654321",
+    summary: "Insights into automating security in CI/CD pipelines.",
+    date: "2024-11-10"
   }
 ];
+
